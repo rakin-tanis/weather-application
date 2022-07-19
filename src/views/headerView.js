1cd91429499c0f39587c231172549c8c861d04a4
akin tanis <akin.tanis.dev@gmail.com>
2022-07-19 13:42:40 +0200

    header text removed

--- src/views/headerView.js
+++ src/views/headerView.js
@@ -3,7 +3,7 @@ import { toHtmlElement } from '../utils/util';
 export const createHeaderElement = () => {
   const htmlStr = String.raw`
         <div class="container" style="height:120px">
-          <h1>WEATHER APPLICATION</h1>
+          
         </div>
     `;
 
